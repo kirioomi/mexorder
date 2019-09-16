0d9a971907b90c296b3ce4cd16c357c31b072f7d
kirioomi <42682576+kirioomi@users.noreply.github.com>
2019-09-17 03:42:46 +0900

    Update Form1.cs

--- mexorder/mexorder/Form1.cs
+++ mexorder/mexorder/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Serilog;
+using Serilog.Events;
+
 namespace mexorder
 {
     public partial class Form1 : Form
@@ -15,6 +18,22 @@ namespace mexorder
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            initLogging();
+        }
+
+        private void initLogging()
+        {
+            var executingDir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            var logPath = Path.Combine(executingDir, "logs", DateTime.UtcNow.ToString("MM-dd-yyyy") + ".log");
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.File(logPath)
+                .WriteTo.Console(LogEventLevel.Information)
+                .CreateLogger();
         }
     }
 }
